e17ffeb8d9a2185b62b872bd6e17f8ea8cf739c4
wvwnumam <wvwn@protonmail.ch>
2020-08-10 15:01:35 +0000

    Migrasi ke MySQL

--- mysite/urls.py
+++ mysite/urls.py
@@ -3,7 +3,7 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('django-admin/', admin.site.urls),
     path('', views.index),
     path('blog/', include('blog.urls'))
 ]
